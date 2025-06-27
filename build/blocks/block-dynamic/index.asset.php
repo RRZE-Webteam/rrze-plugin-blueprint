6323bf1975541021a063fddadbfae6de8e8bbba9
Rolf <rolf.v.d.forst@fau.de>
2025-06-27 12:43:42 +0200

    Add build files

---
+++ build/blocks/block-dynamic/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd71e773f164bf00bc9e4');
---
+++ build/blocks/block-static/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '59209ab65bef0a903d7e');
