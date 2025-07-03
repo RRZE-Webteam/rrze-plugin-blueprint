c94bd9608b0bc99a00be4c03d3376e861d701862
Rolf <rolf.v.d.forst@fau.de>
2025-07-03 16:34:34 +0200

    Update Shortcode.php

--- includes/Common/Shortcode/Shortcode.php
+++ includes/Common/Shortcode/Shortcode.php
@@ -8,10 +8,13 @@ defined('ABSPATH') || exit;
  * Generic Shortcode registration class.
  *
  * Usage:
- * new Shortcode('shortcode_tag', function($atts, $content, $tag) {
- *     // Your shortcode logic here
- *     return 'Hello World!';
- * });
+ * $shortcode = new Shortcode('my_shortcode');
+ * To register the shortcode, you can hook it into the 'init' action:
+ * $shortcode->register(fn($atts, $content = null, $tag = '') =>
+ *     '<div class="my-shortcode">' . esc_html($atts['title']) . '</div>'
+ * );
+ *
+ * @package RRZE\PluginBlueprint\Common\Shortcode
  */
 class Shortcode
 {
@@ -23,42 +26,33 @@ class Shortcode
     protected string $tag;
 
     /**
-     * The callback function for the shortcode.
-     *
-     * This should be a callable that accepts three parameters:
-     * - $atts (array): The shortcode attributes.
-     * - $content (string): The content inside the shortcode.
-     * - $tag (string): The shortcode tag.
-     *
-     * @var callable
-     */
-    protected $callback;
-
-    /**
      * Constructor.
+     * Initializes the shortcode with a tag.
+     * The tag is used to identify the shortcode in the content.
      *
-     * @param string   $tag      The shortcode tag.
-     * @param callable $callback The callback function for the shortcode.
+     * @param string $tag The shortcode tag.
      * @return void
      */
-    public function __construct(string $tag, callable $callback)
+    public function __construct(string $tag)
     {
         $this->tag = $tag;
-        $this->callback = $callback;
-
-        add_action('init', [$this, 'register']);
     }
 
     /**
      * Registers the shortcode.
      *
-     * This method hooks into the 'init' action to register the shortcode.
-     * It uses the add_shortcode function to associate the tag with the callback.
+     * It uses the add_shortcode function to associate the tag with the callback function.
+     * This should be a callable that accepts three parameters:
+     * - $atts (array): The shortcode attributes.
+     * - $content (string): The content inside the shortcode.
+     * - $tag (string): The shortcode tag.
+     * This method should be called during the 'init' action hook to ensure that WordPress is ready to register shortcodes.
      *
+     * @param callable $callback The callback function for the shortcode.
      * @return void
      */
-    public function register()
+    public function register(callable $callback)
     {
-        add_shortcode($this->tag, $this->callback);
+        add_shortcode($this->tag, $callback);
     }
 }
